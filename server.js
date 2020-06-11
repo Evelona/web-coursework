daadc36e2ef32cdac5fec3f70fd10a74b05351d1
Evelona <38008344+Evelona@users.noreply.github.com>
2020-06-11 07:21:16 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express"),
+http = require("http"),
+mongoose = require("mongoose"),
+app = express()
+
+app.use(express.static(__dirname + "/client"));
+app.use('/admin', express.static(__dirname + '/client/student.html'));
+app.use(express.urlencoded());
+
+http.createServer(app).listen(3000);
+
+app.post("/todos", function (req, res) {
+    // сейчас объект сохраняется в req.body
+    var newToDo = req.body;
+    console.log(newToDo);
+    toDos.push(newToDo);
+    // отправляем простой объект
+    res.json({"message":"Вы размещаетесь на сервере!"});
+});
